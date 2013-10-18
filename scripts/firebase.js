1f4c2f41e37e6671ecca716fd4323965efc11602
Ajay Shibu <ajay.shibu.ben@gmail.com>
2013-10-18 21:22:10 +0530

    Initial user works

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,11 +1,25 @@
 		
-
-		var firebaseVar= new Firebase('https://the-spellcraft.firebaseio.com');
+	
+	var firebaseVar = new Firebase('https://the-spellcraft.firebaseio.com');
+	var auth = new FirebaseSimpleLogin(firebaseVar, function (error, user) {
+					  if (error) {
+  									  // an error occurred while attempting login
+    								alert(error);
+ 									 } else if (user) {
+  										  // user authenticated with Firebase
+  											  alert('User ID: ' + user.id + ', Provider: ' + user.provider);
+ 												 } else {
+ 															   // user is logged out
+ 													 }
+																});
 		
+	
+	var instanceVar = firebaseVar.child('instances').push();
+	var chatVar = instanceVar.child('chat');
+	var player = instanceVar.child('player');
+	var enemy = instanceVar.child('enemy');
+	var events = instanceVar.child('events');
 
-
-
-		var chatVar = firebaseVar.child('chat');
 		chatVar.on('child_added',function (snapshot) { var message = snapshot.val();
 														message = message.text;
 														addChatMessage(message);
