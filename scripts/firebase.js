66178c35af7c182422e3785e6bbce42433a27b60
ken259 <kevin.t.jayan@gmail.com>
2013-10-23 23:30:03 +0530

    Basic layout for index

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,43 +1,105 @@
 		
 	
-	var firebaseVar = new Firebase('https://the-spellcraft.firebaseio.com');
-	var auth = new FirebaseSimpleLogin(firebaseVar, function (error, user) {
-	 if (error) {
-    // an error occurred while attempting login
-    alert(error);
-  } else if (user) {
-    // user authenticated with Firebase
-    alert('User ID: ' + user.id + ', Provider: ' + user.provider);
-  } else {
-    // user is logged out
-  }
-	});
-	
-	
-	var instanceVar = firebaseVar.child('instances').push();
-	var chatVar = instanceVar.child('chat');
-	var player = instanceVar.child('player');
-	var enemy = instanceVar.child('enemy');
-	var events = instanceVar.child('events');
+var firebaseVar = new Firebase('https://the-spellcraft.firebaseio.com/');
+var auth = new FirebaseSimpleLogin(firebaseVar, function (error, user) {
 
-		chatVar.on('child_added',function (snapshot) { var message = snapshot.val();
-														message = message.text;
-														addChatMessage(message);
-														});
-						 
-		
-				
-		function saveChatMessage(){
-			if($('#inputBox').val()){
-				var text = $('#inputBox').val();
-				chatVar.push({name: docGet('playerName').innerHTML,text: text});
-				$('#inputBox').val('');
-			}
-		}
+	if (error) {
+	    // an error occurred while attempting login
+	    alert(error);
+	  } else if (user) {
+	    // user authenticated with Firebase
+	   $(document).ready( function(){
 
-		function authLogin(){
-			auth.login('password',{
-				email: $("#useremail").val(),
-				password: $("#userpasswd").val()
+		   $("#logout").show();
+			
+		   $.ajax({
+			    url: "user_home.html"
+		   })
+		   .done( function(data){
+			
+			    $(".login").hide();
+			    $(".content").html(data);
+			    $("#username").append(user.id);
+
+		    });
+	   });
+	} else {
+	    // user is logged out
+	     $(document).ready( function(){
+
+		     $("#logout").hide();
+		     $(".login").show();
+		     $(document).ready( function(){
+				$.ajax({
+			        	url: "home.html"
+		          	})
+		   		.done( function(data){
+			
+				    $(".content").html(data);
+
+		    		});
 			});
-		}
+	     });
+
+	}
+});
+	
+	
+var instanceVar = firebaseVar.child('instances').push();
+var chatVar = instanceVar.child('chat');
+var player = instanceVar.child('player');
+var enemy = instanceVar.child('enemy');
+var events = instanceVar.child('events');
+
+chatVar.on('child_added',function (snapshot){ 
+
+	var message = snapshot.val();
+	message = message.text;
+	addChatMessage(message);
+});
+					
+		
+function saveChatMessage(){
+
+	if($('#inputBox').val()){
+
+		var text = $('#inputBox').val();
+		chatVar.push({name: docGet('playerName').innerHTML,text: text});
+		$('#inputBox').val('');
+	}
+
+}
+
+
+$(document).ready( function(){
+	$("#facebook_login").click( function(){
+
+		auth.login('facebook',{
+		});
+
+	});
+});
+
+
+$(document).ready( function(){
+	$("#email_login").click( function(){
+
+		auth.login('password',{
+			email: $("#useremail").val(),
+			password: $("#userpasswd").val()
+		});
+
+	});
+});
+
+
+$(document).ready( function(){
+	$("#logout").click( function(){
+
+		auth.logout();
+
+	});
+});
+		
+
+		
